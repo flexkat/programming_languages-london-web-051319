e74645e01856417fdfa8e15ea052cc43f57bb796
Catherine Maritz <catherinemaritz1@gmail.com>
2019-05-10 14:43:23 +0100

    reformating hashes with nested loops

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,46 @@
+require 'pry'
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    } 
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |functional_type, info|
+    info.each do |language_name, language_facts|
+      if new_hash.has_key?(language_name) == false
+        new_hash[language_name] = language_facts
+        new_hash[language_name][:style] = []
+      end
+        new_hash[language_name][:style] << functional_type 
+    end 
+  end 
+  new_hash
 end
